fa4ac76949b3db078dbeb9f78ef2cb424ebd2b31
dani <dani.zaghian@gmail.com>
2017-05-03 20:02:48 -0700

    query parameters examples

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -8,4 +8,38 @@ class ExamplesController < ApplicationController
 
   # localhost:3000/query_params_url?my_message=helloworld
 
+  def name
+    @name = params[:query_name]
+    if @name.starts_with?("a") || @name.starts_with?("A")
+      @message = "Hey your name starts with the first letter of the alphabet!"
+    end
+    render "name.html.erb"
+  end
+
+  def guessing_game
+    winning_number = 77
+    @guess = params[:user_guess].to_i
+    if @guess > winning_number
+      @message = "pick lower"
+    elsif @guess < winning_number
+      @message = "pick higher"
+    else
+      @message = "you win"
+    end
+    render "guessing_game.html.erb"
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/query_params_url" => "examples#query_params_method"
+
+  get "/name" => "examples#name"
+
+  get "/guessing_game" => "examples#guessing_game"
 end
